2d7038f62d54ae2a48b75aac1b9d120c340e1d12
SaifASC5 <saifgyro@gmail.com>
2018-07-27 15:49:46 -0400

    ligma

---
+++ notSoRandom.js
@@ -0,0 +1,20 @@
+// function number(){
+//     const decimal = Math.random();
+//     const times22 = decimal * 22
+//     const final = Math.floor (times22)
+//     console.log(final);
+
+// }
+
+//number();
+
+// function randomNumber(num){
+    
+//     conosole.log(num);
+
+//     print("saif");
+
+//}
+
+
+
